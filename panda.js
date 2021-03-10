5ef1725178f7405ebd472d4a385791aa124747c0
freesia-panda <78058270+freesia-panda@users.noreply.github.com>
2021-03-10 12:00:13 +0900

    Update panda.js

--- panda.js
+++ panda.js
@@ -1,5 +1,9 @@
 $(function() {
   $('#btn-wrapper').click(function() {
-     $('img, .contents-item p, .contents-item2 p').fadeIn(5000);
-  });  
+    $('img, .contents-item p, .contents-item2 p').fadeIn(5000);
+  });
+
+  $('.child').click(function() {
+  	$(this).hide();
+  });
 });
