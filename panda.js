91047c6d7af8b35df1cff4b60c5849bbe7588ed6
freesia-panda <78058270+freesia-panda@users.noreply.github.com>
2021-03-21 12:33:16 +0900

    Update panda.js

--- panda.js
+++ panda.js
@@ -6,4 +6,28 @@ $(function() {
   $('.child').click(function() {
   	$(this).hide();
   });
+
+  $('.contents-item, .contents-item2').click(function() {
+    var $answer = $(this).find('.answer');
+    if($answer.hasClass('open')) { 
+      $answer.removeClass('open');
+      // slideUpメソッドを用いて、$answerを隠してください
+      $answer.slideUp();
+
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      
+      
+    } else {
+      $answer.addClass('open'); 
+      // slideDownメソッドを用いて、$answerを表示してください
+      $answer.slideDown();
+
+      
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+     
+      
+    }
+  });
+
+
 });
